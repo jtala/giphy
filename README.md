c6d37d85aa5929313e28a59b622fbe93cd17dd08
Jonathan Talavera <jtalavera@g.ucla.edu>
2018-12-06 20:45:55 -0800

    final finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # giphy
+
+This app is going to create awesome gifs of whatever you choose. Search for awesome cars. But if that's not your thing, search for anything you want.
--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,34 +1,38 @@
 
 
-
-//First button array.
+//first button array.
 
 var brands = ["nissan", "tesla", "toyota", "volkswagen", "porsche", "honda", "jeep", "datsun", "ferrari", "rolls royce"];
 
+// creates buttons dynamically
+function buttonCreator() {
 
+  // making sure no duplicates
+  $("#buttons").empty();
 
-// For loop to create the initial buttons.
-for (i=0; i< brands.length; i++){
+  // for loop to create the initial buttons
+  for (i=0; i< brands.length; i++){
 
-  var b = $('<button/>').text(brands[i]);
-  // giving the buttons identifiable classes.
-  b.addClass("btn-gif");
-  b.addClass("data-brandname");
-  b.attr("data-brandname", brands[i]);
-  
-  // putting it on the html.
-  $("#buttons").append(b);
-    
+    var b = $('<button/>').text(brands[i]);
+    // giving the buttons identifiable classes
+    b.addClass("btn-gif");
+    b.attr("data-brandname", brands[i]);
+
+    // putting on the page.
+    $("#buttons").append(b);   
+  };
+
 };
 
-
-  // on click to create gifs + rating
-  $(".btn-gif").on("click",function(){
+function displayGifs() {
   
-  // creating dynamic ajax
+  
+  $(".btn-gif").on("click",function(){
 
+  // creating dynamic ajax
   var userBrand = $(this).attr("data-brandname");
   var queryURL="https://api.giphy.com/v1/gifs/search?q="+ userBrand +"&api_key=W3a5evec5YVRfHlGQDAehOlDfcfFDo7m";
+
 
   $.ajax({
       url: queryURL,
@@ -37,42 +41,83 @@ for (i=0; i< brands.length; i++){
       
       console.log(response.data);
 
-      // for loop to make 10 of these.
-      for (i=0; i<10; i++){
-        // Creating a gif div
+      // no duplicates
+      $(".gif_dump").empty();
+  
+      // making 15 gifs appear
+      for (i=0; i<15; i++){
+        // giving it identifying classes
         var gifDiv = $("<div class='gifs'>");
-        // This will show the rating
         var newP = $("<p>").text("Rating: " + response.data[i].rating);
-        // Making the image gif.
         var gifImage = $("<img>");
+
+        // gif img source
         gifImage.attr("src", response.data[i].images.original.url);
-        // Putting all together.
+        gifImage.attr("class", "gif");
+
+        //source of imgs which will change depending on click.
+        gifImage.attr("data-still", response.data[i].images.original_still.url);
+        gifImage.attr("data-animate", response.data[i].images.original.url);
+        gifImage.attr("data-state", "animate");
+
+        // putting it on the page.
         var totalGif=  $(gifDiv).prepend(newP, gifImage);
-        // Putting it on the page.
         $(".gif_dump").prepend(totalGif);
       };
+
+      // pause/play functionality.
+      $(".gif").on("click", function() {
+        var state = $(this).attr("data-state"); 
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still"); 
+        }
+      });
   });
 });
-
+};
 // Creating the button that adds gifs.
-
-  $("#add-cars").on("click",function() {
-
+  $("#add-cars").on("click",function(event) {
+  
     event.preventDefault();
 
-    // look into what was typed in the form, extract value and store.
-    var newCar = $("#cars-input").val();
+    // taking what was typed, putting into button
+    var newCar = $("#cars-input").val().trim().toLowerCase();
 
-    // add it onto the array.
-    brands.push(newCar);
+      // making sure there are no duplicates or lowercases.
+      if(jQuery.inArray(newCar, brands) != -1){
+        alert("That's already on the list! Try another one.");
+      }
+      
+      else if (jQuery.inArray(newCar, brands) === -1){
+      // add it onto the array.
+      brands.push(newCar);
 
     // calling function that creates buttons over again
     buttonCreator();
-
+    };
   });
 
-// Making the buttons first appear.
-buttonCreator();
+  //disables button if nothing is present
+  $(document).ready(function(){
+    $("#add-cars").attr('disabled',true);
+    $('#cars-input').keyup(function(){
+        if($(this).val().length !=0)
+            $("#add-cars").attr('disabled', false);            
+        else
+            $("#add-cars").attr('disabled',true);
+    })
+});
+
+  // On click event listener for all the buttons.
+
+  $(document).on("click", ".btn-gif", displayGifs);
 
 
-// To Do: 1 - Add functionality to the submit button. 2-  Styling
+
+  // Making the buttons first appear.
+  buttonCreator();
+  displayGifs();
